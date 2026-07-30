c41452df04ebc4ca4e150e652b39c0a0ebcb14c4
iki1uc <entertrainer@web.de>
2026-07-30 17:47:32 +0200

    Update co.js

--- src/co/co.js
+++ src/co/co.js
@@ -2,10 +2,11 @@ import { PQ_COPILOT } from "../pq/pq-copilot.js";
 import { PP_COPILOT } from "../pp/pp-copilot.js";
 
 export const CO = {
-    boot(respo) {
-        console.log("CO Copilot gestartet…");
+    async boot(respo) {
+        const sdsa = await respo.loadSDSA();
 
-        const sdsa = respo.loadSDSA();
+        sdsa.coDegree = 360;
+        sdsa.coFulfillment = 100;
 
         const pqCopilot = PQ_COPILOT.init(sdsa);
         const ppCopilot = PP_COPILOT.init(sdsa);
@@ -14,7 +15,9 @@ export const CO = {
             sdsa,
             pqCopilot,
             ppCopilot,
-            respo
+            respo,
+            degree: 360,
+            fulfillment: 100
         };
     }
 };
