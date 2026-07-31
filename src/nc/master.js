defa6ab4f9ef0bd7f47fc4eb679d5bcd75ed39e4
iki1uc <entertrainer@web.de>
2026-07-31 03:34:35 +0200

    Update master.js

--- src/nc/master.js
+++ src/nc/master.js
@@ -1,1 +1,34 @@
+import { CO_API } from "./api.js";
+import { CO_STATION } from "./station.js";
+import { CO_RUNTIME } from "./runtime.js";
 
+import { CO_SYSTEM_MAP } from "./KG.js";
+import { CO_ANALYZE } from "./CO.js";
+import { CO_DECIDE } from "./BOOT.js";
+
+import { SDSA } from "../SDSA/station.js";
+import { PQ } from "../PQ/station.js";
+import { PP } from "../PP/station.js";
+
+export const CO = {
+    api: CO_API,
+    station: CO_STATION,
+    runtime: CO_RUNTIME,
+    map: CO_SYSTEM_MAP,
+    analyze: CO_ANALYZE,
+    decide: CO_DECIDE,
+
+    boot(respo) {
+        return {
+            respo,
+            sdsa: SDSA.boot(respo),
+            pqCopilot: PQ.boot(respo),
+            ppCopilot: PP.boot(respo),
+
+            runtime: CO_RUNTIME,
+
+            degree: 360,
+            fulfillment: 100
+        };
+    }
+};
