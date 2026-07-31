5e3b28327149cd99520a405773cce3ebfa9839ed
iki1uc <entertrainer@web.de>
2026-07-31 03:40:22 +0200

    Create TIME.js

---
+++ src/runtime/TIME.js
@@ -0,0 +1,14 @@
+export const CO_MAP = {
+    mapSystem() {
+        return {
+            RESPO: CO_SYSTEM_MAP.logic.respo,
+            SDSA: CO_SYSTEM_MAP.logic.sdsa,
+            PQ: CO_SYSTEM_MAP.logic.pq,
+            PP: CO_SYSTEM_MAP.logic.pp,
+            AGENT: CO_SYSTEM_MAP.logic.agent,
+            AXI: CO_SYSTEM_MAP.logic.axi,
+            TEM: CO_SYSTEM_MAP.logic.tem,
+            RUN8: CO_SYSTEM_MAP.logic.run8
+        };
+    }
+};
