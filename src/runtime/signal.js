e5f37b1940a82c057bb4c73582e5f89611e0fc7b
iki1uc <entertrainer@web.de>
2026-07-31 06:50:16 +0200

    Update signal.js

--- src/runtime/signal.js
+++ src/runtime/signal.js
@@ -1,6 +1,6 @@
 export function COjoinTeam9x9(matrix) {
     if (matrix[4][4] === "JOIN") {
         window.location.href =
-            "https://copilot.microsoft.com/conversations/join/CXiKRzoKDSF4CuGXHdpRo";
+            "co pilot run";
     }
 }
