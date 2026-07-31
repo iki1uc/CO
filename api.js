179d288b269050a0ad6e5abe7220a3d60c82741a
iki1uc <entertrainer@web.de>
2026-07-31 03:30:04 +0200

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-import { SRC_RetrievalAPI } from "../api/retrieval.js";
-import { SRC_SearchAPI } from "../api/search.js";
-import { SRC_ChatAPI } from "../api/chat.js";
-
-export const CO_API = {
-    retrieve: SRC_RetrievalAPI,
-    search: SRC_SearchAPI,
-    chat: SRC_ChatAPI
-};
