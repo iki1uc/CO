cae474df44bb941303e570a6da5ce566b005e577
iki1uc <entertrainer@web.de>
2026-07-31 03:27:08 +0200

    Delete BOOT.js

--- BOOT.js
+++
@@ -1,8 +0,0 @@
-export const CO_DECIDE = {
-    nextAction(systemState) {
-        if (!systemState.respo.ok) return "Stabilisieren";
-        if (systemState.pq.priority > 80) return "Priorität behandeln";
-        if (systemState.pp.optimization < 50) return "Optimieren";
-        return "System OK";
-    }
-};
