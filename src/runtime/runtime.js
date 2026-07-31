73d5ed4ecaf86d2c7b3183221eb657cdcd9fe15b
iki1uc <entertrainer@web.de>
2026-07-31 03:33:20 +0200

    Update runtime.js

--- src/runtime/runtime.js
+++ src/runtime/runtime.js
@@ -1,1 +1,15 @@
+import { CO_STATION } from "./station.js";
 
+export const CO_RUNTIME = {
+    async load(path) {
+        return await CO_STATION.runRetrieve(path);
+    },
+
+    search(query, dataset) {
+        return CO_STATION.runSearch(query, dataset);
+    },
+
+    chat(message) {
+        return CO_STATION.runChat(message);
+    }
+};
