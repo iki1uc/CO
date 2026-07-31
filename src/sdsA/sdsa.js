f4679b5083ebc6ef0aac963e65d16eafbb1fc5c9
iki1uc <entertrainer@web.de>
2026-07-31 07:14:55 +0200

    Update sdsa.js

--- src/sdsA/sdsa.js
+++ src/sdsA/sdsa.js
@@ -1,36 +1,40 @@
 export const SDSA = {
+
     async loadCSV(path) {
         const response = await fetch(path);
+        if (!response.ok) return [];
+
         const text = await response.text();
-
         const lines = text.trim().split("\n");
-        const headers = lines[0].split(",");
 
-        const data = lines.slice(1).map(line => {
+        return lines.slice(1).map(line => {
             const values = line.split(",");
             return {
                 axis: values[0],
-                value: Number(values[1]),
+                value: values[1],
                 degree: 360,
                 fulfillment: 100
             };
         });
-
-        return data;
     },
 
     async loadAll() {
-        const B = await this.loadCSV("B.csv");
-        const H = await this.loadCSV("H.csv");
-        const T = await this.loadCSV("T.csv");
+
+        const AXIOM = await this.loadCSV("axiom-hardware-6.csv");
+        const FEHLER = await this.loadCSV("fehler-ursache-5.csv");
+        const PIPE = await this.loadCSV("pipeline-station-11.csv");
+        const RESPO = await this.loadCSV("respo-master.csv");
+        const STATUS = await this.loadCSV("system-status.csv");
 
         return {
             axes: 81,
             vector: "360°",
             fulfillment: "100%",
-            B,
-            H,
-            T
+            AXIOM,
+            FEHLER,
+            PIPE,
+            RESPO,
+            STATUS
         };
     }
 };
