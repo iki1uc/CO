7d34a7fd9de0a6876b2918d7ac8a2ea30aa65d95
iki1uc <entertrainer@web.de>
2026-07-31 05:20:24 +0200

    Refactor CO_STATION to use ORBIT_CACHE and update UI

--- src/station/station.js
+++ src/station/station.js
@@ -1,15 +1,12 @@
-import { CO_API } from "./api.js";
+import { ORBIT_CACHE } from "./orbit.js";
+import { updateOrbitUI } from "./orbit.js";
 
-CO_STATION.api = CO_API;
+trigger("agentOn", () => {
+    ORBIT_CACHE.AGENT = "XI";
+    updateOrbitUI();
+});
 
-CO_STATION.runRetrieve = async function(path) {
-    return await this.api.retrieve(path);
-};
-
-CO_STATION.runSearch = function(query, dataset) {
-    return this.api.search(query, dataset);
-};
-
-CO_STATION.runChat = function(message) {
-    return this.api.chat(message, { station: "CO" });
-};
+trigger("agentOff", () => {
+    ORBIT_CACHE.AGENT = "IX";
+    updateOrbitUI();
+});
