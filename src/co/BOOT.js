22ea3cdf6089f3993aea2cfb63acdf7fa7474c54
iki1uc <entertrainer@web.de>
2026-07-31 03:27:00 +0200

    Update BOOT.js

--- src/co/BOOT.js
+++ src/co/BOOT.js
@@ -1,1 +1,8 @@
-
+export const CO_DECIDE = {
+    nextAction(systemState) {
+        if (!systemState.respo.ok) return "Stabilisieren";
+        if (systemState.pq.priority > 80) return "Priorität behandeln";
+        if (systemState.pp.optimization < 50) return "Optimieren";
+        return "System OK";
+    }
+};
